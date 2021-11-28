7b56fbfc7c1e7977ecb1255ae00d8e69d56816af
Richard Sithole <richardsit@gmail.com>
2021-11-28 07:23:26 +0100

    Add GameBoardComponent

--- libs/ui-shared/game-board/src/lib/ui-shared-game-board.module.ts
+++ libs/ui-shared/game-board/src/lib/ui-shared-game-board.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameBoardComponent } from './game-board/game-board.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    GameBoardComponent
+  ],
+  exports: [
+    GameBoardComponent
+  ],
 })
 export class UiSharedGameBoardModule {}
