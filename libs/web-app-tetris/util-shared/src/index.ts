41324a71aa84789a35f4266d54bc571165db066e
Richard Sithole <richardsit@gmail.com>
2021-11-28 10:17:15 +0100

    Update util-shared public api

--- libs/web-app-tetris/util-shared/src/index.ts
+++ libs/web-app-tetris/util-shared/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/web-app-tetris-util-shared';
+export * from './lib/piece-util';
+export * from './lib/constants/web-app-tetris-util-constants';
+export * from './lib/services/game-engine.service';
