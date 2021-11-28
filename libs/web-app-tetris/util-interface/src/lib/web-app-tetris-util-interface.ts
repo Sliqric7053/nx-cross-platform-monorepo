1026d8b9c887e23d5967dd8a3bcb922e9606dafd
Richard Sithole <richardsit@gmail.com>
2021-11-28 10:15:20 +0100

    Modularize IPiece util

--- libs/web-app-tetris/util-interface/src/lib/web-app-tetris-util-interface.ts
+++ libs/web-app-tetris/util-interface/src/lib/web-app-tetris-util-interface.ts
@@ -1,3 +1,10 @@
 export function webAppTetrisUtilInterface(): string {
   return 'web-app-tetris-util-interface';
 }
+
+export interface IPiece {
+  x: number;
+  y: number;
+  color: string;
+  shape: number[][];
+}
