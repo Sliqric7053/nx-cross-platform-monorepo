9d02948ddcdc977f263b2192ae40021313a14cf6
Richard Sithole <richardsit@gmail.com>
2021-11-28 08:49:05 +0100

    Add webAppTetrisUtilShared lib

---
+++ libs/web-app-tetris/util-shared/README.md
@@ -0,0 +1,7 @@
+# web-app-tetris-util-shared
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-app-tetris-util-shared` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/web-app-tetris/util-shared/src/lib/web-app-tetris-util-shared.spec.ts
@@ -0,0 +1,7 @@
+import { webAppTetrisUtilShared } from './web-app-tetris-util-shared';
+
+describe('webAppTetrisUtilShared', () => {
+  it('should work', () => {
+    expect(webAppTetrisUtilShared()).toEqual('web-app-tetris-util-shared');
+  });
+});
