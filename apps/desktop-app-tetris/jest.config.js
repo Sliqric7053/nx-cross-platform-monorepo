eb2f3b163f1f8fe3a2ca217f80f8d89a546cbbbe
Richard Sithole <richardsit@gmail.com>
2021-11-29 08:20:08 +0100

    Add desktop app

---
+++ apps/desktop-app-tetris/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'desktop-app-tetris',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/desktop-app-tetris',
+};
---
+++ apps/desktop-app-tetris/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'web-app-tetris';
+export const electronAppName = 'desktop-app-tetris';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
