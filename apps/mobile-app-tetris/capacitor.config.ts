99faa9b642f5b51f4a94b16e4415b23eab1fa174
Richard Sithole <richardsit@gmail.com>
2021-11-29 10:29:14 +0100

    Create @nxtend/capacitor project

---
+++ apps/mobile-app-tetris/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mobile.tetris',
+  appName: 'tetris',
+  webDir: '../../dist/apps/mobile-app-tetris',
+  bundledWebRuntime: false,
+};
+
+export default config;
