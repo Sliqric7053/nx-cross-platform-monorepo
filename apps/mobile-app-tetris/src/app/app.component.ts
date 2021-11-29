f3d847f60d6ddfb950d6d7d4017b2b48a550b82c
Richard Sithole <richardsit@gmail.com>
2021-11-29 08:27:05 +0100

    Add mobile app

---
+++ apps/mobile-app-tetris/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-cross-platform-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mobile-app-tetris';
+}
